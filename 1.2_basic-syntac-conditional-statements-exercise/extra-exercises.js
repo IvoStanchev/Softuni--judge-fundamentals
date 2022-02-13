f3afc8e8a57fa0c64f8f7634ccca948b5a03aecf
Ivo <73983660+IvoStanchev@users.noreply.github.com>
2022-02-13 23:48:31 +0200

    2 English name of the last digit complete

--- 1.2_basic-syntac-conditional-statements-exercise/extra-exercises.js
+++ 1.2_basic-syntac-conditional-statements-exercise/extra-exercises.js
@@ -10,3 +10,28 @@ function sortNumbers(num1, num2, num3) {
 }
 
 sortNumbers(-2, 1, 3);
+
+/* 2 English name of the last digit */
+
+function numberToEnglish(numberToTransform) {
+	const numbers = {
+		0: 'zero',
+		1: 'one',
+		2: 'two',
+		3: 'three',
+		4: 'four',
+		5: 'five',
+		6: 'six',
+		7: 'seven',
+		8: 'eight',
+		9: 'nine',
+	};
+	const lastDigit = numberToTransform.toString().split('').pop();
+	for (const key in numbers) {
+		if (key === lastDigit) {
+			console.log(numbers[key]);
+		}
+	}
+}
+
+numberToEnglish(2423);
