e3305f72c49c83015f8ea654d2400aaf54e828f9
Ivo <73983660+IvoStanchev@users.noreply.github.com>
2022-02-15 20:30:51 +0200

    2. Concatenate named completed

--- 2.1_data_types_and_vars/lab.js
+++ 2.1_data_types_and_vars/lab.js
@@ -1,8 +1,8 @@
 /* 1. Echo type
-[ ] Write a function that receives one parameter
-[ ] Check for the type of that parameter
-[ ] If the parameter is string or number print its value
-[ ] else print a predefined string 
+[x] Write a function that receives one parameter
+[x] Check for the type of that parameter
+[x] If the parameter is string or number print its value
+[x] else print a predefined string 
 */
 
 function typeChecker(inputParameter) {
@@ -16,3 +16,14 @@ function typeChecker(inputParameter) {
 		console.log(`Parameter is not suitable for printing`);
 	}
 }
+
+/* 2. Concatenate Named 
+  [ ] Write a function that receives two strings ana a delimiter as a parameter
+  [ ] Console log the two names with the paramenter between them
+*/
+
+function concatenateStrings(stringOne, stringTwo, delimiter) {
+	console.log(`${stringOne}${delimiter}${stringTwo}`);
+}
+
+concatenateStrings('John', 'Smith', '->');
