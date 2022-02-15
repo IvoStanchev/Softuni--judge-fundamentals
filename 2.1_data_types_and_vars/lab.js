240d14c0d5f605150dad7ef5c7c8ab6ebfc970a4
Ivo <73983660+IvoStanchev@users.noreply.github.com>
2022-02-15 20:48:40 +0200

    3. Right place completed

--- 2.1_data_types_and_vars/lab.js
+++ 2.1_data_types_and_vars/lab.js
@@ -18,8 +18,8 @@ function typeChecker(inputParameter) {
 }
 
 /* 2. Concatenate Named 
-  [ ] Write a function that receives two strings ana a delimiter as a parameter
-  [ ] Console log the two names with the paramenter between them
+  [x] Write a function that receives two strings ana a delimiter as a parameter
+  [x] Console log the two names with the parameter between them
 */
 
 function concatenateStrings(stringOne, stringTwo, delimiter) {
@@ -27,3 +27,22 @@ function concatenateStrings(stringOne, stringTwo, delimiter) {
 }
 
 concatenateStrings('John', 'Smith', '->');
+
+/* 3. Right place
+[x] Create a function that receives three strings as parameters
+[x] The first argument of the function contains an underscore that needs to be replaced with the second function argument, which is a character. Replace the underscore and save the modified string
+[x] Compare the modified string with the 3rd function argument
+[x] Log Matcher or Not Matched depending on wether the strings match or not
+*/
+
+function replaceAndcompare(stringToModify, replacementChar, stringToCompare) {
+	// modify the string by replacing the underscore with the given replacement char
+	let modfifiedString = stringToModify.replace('_', replacementChar);
+
+	// Practice ternary operators
+	modfifiedString === stringToCompare
+		? console.log('Matched')
+		: console.log('Not Mached');
+}
+replaceAndcompare('Str_ng', 'I', 'Strong');
+replaceAndcompare('Str_ng', 'i', 'String');
