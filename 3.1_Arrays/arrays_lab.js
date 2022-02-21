f0245631bb595000547e56e3c7f3635e383ab893
Ivo <73983660+IvoStanchev@users.noreply.github.com>
2022-02-21 17:05:33 +0200

    3. Reverse an array of numbers & 4. Reverse In place completed

--- 3.1_Arrays/arrays_lab.js
+++ 3.1_Arrays/arrays_lab.js
@@ -15,10 +15,10 @@ function sumFirstAndLast(arrayOfNumbers) {
 sumFirstAndLast([20, 30, 40]);
 
 /* 2. Day of Week
- * [] Write a function that receives a number
- * [] Create an array holding all days of the Week
- * [] Create a variable holding the requested day. The day can be found with with array index - 1
- * [] Log the day.
+ * [x] Write a function that receives a number
+ * [x] Create an array holding all days of the Week
+ * [x] Create a variable holding the requested day. The day can be found with with array index - 1
+ * [x] Log the day.
  */
 
 function dayOfWeek(dayNumber) {
@@ -37,3 +37,41 @@ function dayOfWeek(dayNumber) {
 }
 
 dayOfWeek(8);
+
+/* 3. Reverse an Array of Numbers
+ * [x] Create a function that receives an array and a number
+ * [x] Create a new array to hold the reversed one.
+ * [x] Splice the first N number of items
+ * [x] Initiate a for loop and push the items in the new one in reverse order.
+ * [x] Log the result
+ */
+
+function reverseAndShorten(index, arr) {
+	let reversedArray = [];
+	let numberOfItemsToReverse = arr.splice(0, index);
+
+	for (let i = numberOfItemsToReverse.length - 1; i >= 0; i--) {
+		reversedArray.push(numberOfItemsToReverse[i]);
+	}
+
+	console.log(reversedArray.toString().split(',').join(' '));
+}
+
+reverseAndShorten(4, [-1, 20, 99, 5]);
+
+/* 4. Reverse In Place
+ * [x] Create a function that receives an array
+ * [x] Create a variable that will hold the result string
+ * [x] Create a for loop that starts from the length of the array and loops backwards
+ * [x] Add the values to the result string with string interpolation and a space, trim the result and log it to the console.
+ */
+
+function reverseArray(arr) {
+	let output = '';
+	for (let i = arr.length - 1; i >= 0; i--) {
+		output += `${arr[i]} `;
+	}
+	console.log(output.trim());
+}
+
+reverseArray(['a', 'b', 'c', 'd', 'e']);
